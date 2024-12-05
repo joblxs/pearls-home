3e02ec952bae7b4ced2bd3467710fba8c86f235c
lxs <lxs@lxs>
2024-12-05 17:08:27 +0800

    提取时间格式为单独组件

--- src/components/module/HotHome.vue
+++ src/components/module/HotHome.vue
@@ -16,7 +16,7 @@
               </ul>
             </template>
             <template v-slot:footer>
-              <time :datetime="item.updateTime" :title="item.updateTime" style="display: inline;">更新于{{ item.formatPast }}</time>
+              <time :datetime="item.updateTime" :title="item.updateTime" style="display: inline;">更新于<TimeFormat :date="item.updateTime" /></time>
             </template>
           </lay-card>
         </lay-col>
@@ -28,8 +28,12 @@
 <script>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import TimeFormat from '@/utils/TimeFormat.vue';
 
 export default {
+  components: {
+    TimeFormat
+  },
   setup() {
     // 使用ref创建响应式数据
     const hotList = ref([]);
@@ -53,14 +57,12 @@ export default {
       try {
         // 并行请求所有平台的数据
         const results = await Promise.allSettled(platforms.map(platform => fetchHotList(platform)));
-        console.log('1111', results)
         // 将结果存储到 hotList 中
         hotList.value = results
           .filter(result => result.status === 'fulfilled') // 只保留成功的请求
           .map((result, index) => ({
             title: platforms[index], // 使用平台名称作为标题
             updateTime: result.value.updateTime, // 从 API 获取的更新时间
-            formatPast: formatPast(result.value.updateTime), // 格式化从 API 获取的更新时间
             list: result.value.data // 从 API 获取的数据列表
           }));
       } catch (error) {
@@ -69,86 +71,9 @@ export default {
       }
     });
 
-    const formatPast = (date, type = "default", zeroFillFlag = true) => {
-      // 定义countTime变量，用于存储计算后的数据
-      let countTime;
-      // 获取当前时间戳
-      let time = new Date().getTime();
-      // 转换传入参数为时间戳
-      let afferentTime = new Date(date).getTime();
-      // 当前时间戳 - 传入时间戳
-      time = Number.parseInt(`${time - afferentTime}`);
-      let typeList = ['default', '-', '/', '.', '年月日', '月日', '年']
-      if (time < 0 || (typeof time === 'number' && isNaN(time)) || !date) {
-        // 时间参数存在问题 此处alert仅做错误示例，实际应用以场景需求为准
-        return alert("请查看时间参数是否满足规则:\n1、时间参数为必填参数且为过去时间 \n2、时间参数不合法(参考date参数规则)");
-      } else if ((zeroFillFlag && typeof zeroFillFlag !== 'boolean') || (type && !typeList.includes(type))) {
-        // 补零参数或格式参数存在问题 此处alert仅做错误示例，实际应用以场景需求为准
-        return alert("请查看补零参数/格式参数是否满足规则：\n1、补零参数为布尔值类型 \n2、格式参数不合法(参考type参数规则)");
-      } else if (time < 10000) {
-        // 10秒内
-        return "刚刚";
-      } else if (time < 60000) {
-        // 超过10秒少于1分钟内
-        countTime = Math.floor(time / 1000);
-        return `${countTime}秒前`;
-      } else if (time < 3600000) {
-        // 超过1分钟少于1小时
-        countTime = Math.floor(time / 60000);
-        return `${countTime}分钟前`;
-      } else if (time < 86400000) {
-        // 超过1小时少于24小时
-        countTime = Math.floor(time / 3600000);
-        return `${countTime}小时前`;
-      } else if (time >= 86400000 && type == "default") {
-        // 超过二十四小时（一天）且格式参数为默认"default"
-        countTime = Math.floor(time / 86400000);
-        //大于等于365天
-        if (countTime >= 365) {
-          return `${Math.floor(countTime / 365)}年前`;
-        }
-        //大于等于30天
-        if (countTime >= 30) {
-          return `${Math.floor(countTime / 30)}个月前`;
-        }
-        return `${countTime}天前`;
-      } else {
-        // 一天（24小时）以上且格式不为"default"则按传入格式参数显示不同格式
-        // 数字补零
-        let Y = new Date(date).getFullYear();
-        let M = new Date(date).getMonth() + 1;
-        let zeroFillM = M > 9 ? M : "0" + M;
-        let D = new Date(date).getDate();
-        let zeroFillD = D > 9 ? D : "0" + D;
-        // 传入格式为"-" "/" "."
-        if (type == "-" || type == "/" || type == ".") {
-          return zeroFillFlag
-            ? Y + type + zeroFillM + type + zeroFillD
-            : Y + type + M + type + D;
-        }
-        // 传入格式为"年月日"
-        if (type == "年月日") {
-          return zeroFillFlag
-            ? Y + type[0] + zeroFillM + type[1] + zeroFillD + type[2]
-            : Y + type[0] + M + type[1] + D + type[2];
-        }
-        // 传入格式为"月日"
-        if (type == "月日") {
-          return zeroFillFlag
-            ? zeroFillM + type[0] + zeroFillD + type[1]
-            : M + type[0] + D + type[1]
-        }
-        // 传入格式为"年"
-        if (type == "年") {
-          return Y + type
-        }
-
-      }
-    };
-
     // 返回响应式数据
     return {
-      hotList
+      hotList, TimeFormat
     };
   }
 }
@@ -176,7 +101,7 @@ h1:after {
 }
 
 .hot {
-  max-width: 90vw;
+  max-width: 80vw;
   border-radius: 1%;
   margin: 0 auto;
   background-image: url("@/assets/images/shan.webp");
