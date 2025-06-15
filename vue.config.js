889e32ebb271e275f468a718a01eb9b36bfc1705
lxs <lxs@lxs.localdomain>
2025-06-15 12:55:12 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './', // 设置公共路径
   devServer: {
     client: {
       overlay: {
         errors: false,
         warnings: false
       }
-    }
+    },
+    historyApiFallback: true // 开启路由回退
   }
-})
+});
